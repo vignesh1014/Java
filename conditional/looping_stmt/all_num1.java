2e291f508d5a829dfbf33cb20b10122ce5112349
vignesh1014 <139304057+vignesh1014@users.noreply.github.com>
2023-07-12 12:04:09 +0530

    Add files via upload

---
+++ conditional/looping_stmt/all_num1.java
@@ -0,0 +1,7 @@
+class all_num1{
+    public static void main(String[] args) {
+       //to print all number from 1 to 100
+       for(int i=1;i<=100;i++)
+       System.out.print(i+" ");
+    }
+}
---
+++ conditional/looping_stmt/alter0_num1.java
@@ -0,0 +1,7 @@
+class alter0_num1{
+    public static void main(String[] args) {
+       //to print alternative number starting from 0 to 100
+       for(int i=0;i<=100;i=i+2)
+       System.out.print(i+" ");
+    }
+}
