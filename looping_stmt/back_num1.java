193e318e294096e4cd73380d6b267195bc4fc1e2
vignesh1014 <139304057+vignesh1014@users.noreply.github.com>
2023-07-12 12:05:03 +0530

    Add files via upload

---
+++ looping_stmt/back_num1.java
@@ -0,0 +1,7 @@
+class back_num1{
+    public static void main(String[] args) {
+       //to print all number backwards from 100 to 0
+       for(int i=100;i>=0;i--)
+       System.out.print(i+" ");
+    }
+}
